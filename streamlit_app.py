70db21856a5ba4c0f9b0a852eba0ce6c4916058e
DanielaPalacian <38717109+DanielaPalacian@users.noreply.github.com>
2026-08-10 11:36:51 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ if ingredients_list:
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")
     sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-st.write(ingredients_string)
+    st.write(ingredients_string)
 st.stop()
 
 name_on_order = title
