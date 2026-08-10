1cbf692d08708b2ba4502b6898734a6895f5dccf
DanielaPalacian <38717109+DanielaPalacian@users.noreply.github.com>
2026-08-10 11:11:16 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,15 +35,15 @@ st.write('This oreder is made by ', title)
 cnx=st.connection("snowflake")
 session= cnx.session()
 
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
+
 ingredients_list = st.multiselect(
     'Choose up to 5 incredients'
     ,my_dataframe
     ,max_selections=5
 )
-
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
 
 #pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
